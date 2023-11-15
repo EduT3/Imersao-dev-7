9e65dd37a3616f9fca0e7b431719a9553d541d82
Eduardo Paulo Tomilhero <Eduardo_tomi@hotmail.com>
2023-11-15 18:07:00 -0300

    hotfix
    aplicando href para a 3 aula e arrumando texto da aula 2

--- aula 2/script.js
+++ aula 2/script.js
@@ -2,7 +2,7 @@ var numeroSecreto = parseInt(Math.random() * 100);
 var vidas = 10;
 
 while (chute != numeroSecreto && vidas > 0) {
-  var chute = prompt("Digite o número entre 1 e 100.... Você tem 10 chances");
+  var chute = prompt("Digite o número entre 1 e 100.... Você tem " + vidas + " chances");
 
   if (chute == numeroSecreto) {
     alert("Acertou!");
