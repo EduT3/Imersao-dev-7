755cc0f1b132d0e6ad2848f06d64d3f354eff84d
Eduardo Paulo Tomilhero <Eduardo_tomi@hotmail.com>
2023-11-15 03:12:05 -0300

    aula 2
    jogo de adivinhação

---
+++ aula 2/script.js
@@ -0,0 +1,16 @@
+var numeroSecreto = parseInt(Math.random() * 100);
+var vidas = 10;
+
+while (chute != numeroSecreto && vidas > 0) {
+  var chute = prompt("Digite o número entre 1 e 100.... Você tem 10 chances");
+
+  if (chute == numeroSecreto) {
+    alert("Acertou!");
+  } else if (chute < numeroSecreto) {
+    vidas = vidas - 1;
+    alert("Errou! o número secreto é maior. Você tem " + vidas);
+  } else if (chute > numeroSecreto) {
+      vidas = vidas - 1;
+      alert("Errou! o número secreto é menor. Você tem " + vidas);
+  }
+}
